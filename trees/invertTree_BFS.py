f6d5eb9d02f0f3646a9b53d060eb24245db5a4f3
Armin <arminahmadkhaniha.ee@gmail.com>
2025-11-21 18:34:05 -0500

    add invertTree_BFS

---
+++ trees/invertTree_BFS.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return root
+
+
+# Test
+from invertTree_DFS import build_tree, tree_to_list
+
+
+sol = Solution()
+tree = build_tree([4,2,7,1,3,6,9])
+inverted = sol.invertTree(tree)
+print(tree_to_list(inverted))
