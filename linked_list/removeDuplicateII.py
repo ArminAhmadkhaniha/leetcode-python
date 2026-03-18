bb2c5047d61bac526cfa92703593d01f3c1c32c3
Armin <arminahmadkhaniha.ee@gmail.com>
2026-03-17 20:16:35 -0400

    add remove duplicate II from list

---
+++ linked_list/removeDuplicateII.py
@@ -0,0 +1,22 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(None, head)
+        prev = dummy
+        cur = head
+
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                while cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                prev.next = cur.next
+            else:
+                prev = cur
+            
+            cur = cur.next
+
+        return dummy.next
