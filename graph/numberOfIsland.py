b0e242a376c71daccd1811e7ae1020884eac9645
Armin <arminahmadkhaniha.ee@gmail.com>
2026-06-18 15:37:42 -0400

    add my version to number of island

--- graph/numberOfIsland.py
+++ graph/numberOfIsland.py
@@ -64,6 +64,45 @@ class Solution:
 #                     islands += 1
                 
 #         return islands
+
+
+
+# my version
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         visited = set()
+#         rows = len(grid)
+#         cols = len(grid[0])
+#         directions = [(1,0), (0,1), (-1, 0), (0,-1)]
+#         count = 0
+
+#         def get_nei(r,c):
+#             nei = []
+#             for dr, dc in directions:
+#                 nr, nc = r + dr, c + dc
+#                 if 0<= nr < rows and 0<= nc < cols:
+#                     nei.append((nr,nc))
+#             return nei
+        
+#         def dfs(r,c):
+#             if (r,c) in visited or grid[r][c] == "0":
+#                 return
+#             visited.add((r,c))
+#             for nr , nc in get_nei(r,c):
+#                 dfs(nr,nc)
+
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == "1" and (r,c) not in visited:
+#                     dfs(r,c)
+#                     count += 1
+                
+#         return count
+
+
+
+
+        
         
         
 
