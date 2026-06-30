309010644c1a670685435c1f7675d6917877059f
Armin <arminahmadkhaniha.ee@gmail.com>
2026-06-30 13:21:00 -0400

    add another eval. eq.

--- graph/evaluateeq.py
+++ graph/evaluateeq.py
@@ -32,6 +32,36 @@ class Solution:
         return res
 
 
+# class Solution:
+#     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+
+#         graph = collections.defaultdict(list)
+
+#         for i, (a, b) in enumerate(equations):
+#             graph[a].append([b, values[i]])
+#             graph[b].append([a, 1 / values[i]])
+
+#         def dfs(src, tar, visited):
+#             if src == tar:
+#                 return 1
+#             visited.add(src)
+#             for nei , weight in graph[src]:
+#                 if nei not in visited:
+#                     res = dfs(nei, tar, visited)
+#                     if res != -1:
+#                         return res * weight
+#             return -1
+
+#         res = []
+#         for src , tar in queries:
+#             if src not in graph or tar not in graph:
+#                 res.append(-1)
+#             else:
+#                 res.append(dfs(src, tar, set()))
+#         return res
+
+        
+
 
 
         
